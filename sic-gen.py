08ac6ddb7a5f023541c6fddb15f5dddd840e6c4b
Pawel Drozdowski <pawel.drozdowski@h-da.de>
2017-08-02 09:31:03 +0200

    add multiprocessing

--- sic-gen.py
+++ sic-gen.py
@@ -8,19 +8,20 @@ import math
 from pathlib import Path
 import cv2
 import os
-from typing import Union, Tuple
 from multiprocessing import Pool, cpu_count, set_start_method
 from timeit import default_timer as timer
 from template import Template
 
 median_filter_rows = 2
+reference_generation_hd = 0.4625
 noise_ic = Template.from_image(Path("noise_ic.bmp"))
+generated_directory = Path("generated")
+subjects = 4
+cpus = 4
 
 with open("exp.json", "r") as f:
 	exp_overlaps = json.load(f)
 	exp_overlaps = {float(k):float(v) for k,v in exp_overlaps.items()}
-	
-
 
 class IrisCodeGenerator(object):
 	def __init__(self, to_produce, probes_per_subject):
@@ -35,7 +36,48 @@ class IrisCodeGenerator(object):
 
 	def __next__(self) -> np.ndarray:
 		'''Produces one iris-code.'''
-		return []
+		while self._produced < self._to_produce:
+			temp_ic = Template.create(32, 512, 6.5, 0.25)
+			temp_ic.to_image(Path("test.bmp"))
+			temp_ic.shift(np.random.randint(10, 512 // 2))
+			temp_ic.initial_zigzag()
+
+			seqs = temp_ic.find_sequences_of_all(1)
+			target_hd = float(weibull(3))
+			exp_overlap = expected_overlap(target_hd)
+			i_hd = (target_hd + exp_overlap) / 2
+			b_hd = reference_generation_hd - i_hd
+
+			print ("T-HD:", target_hd)
+			print ("I-HD:", i_hd)
+			print ("B-HD:", b_hd)
+			temp_ic = flip_barcode(temp_ic, b_hd)
+
+			reference, probe, a_hd = flip_templates(temp_ic, target_hd)
+
+			noise_hd = target_hd - a_hd
+			print ("A-HD:", a_hd)
+			print ("BR-HD:", temp_ic.hamming_distance(reference)[0])
+			print ("BP-HD:", temp_ic.hamming_distance(probe)[0])
+			print ("N-HD:", noise_hd)
+			arch_side = np.random.choice(("l", "r", None), p=(0.5, 0.5, 0.0))
+			print ("AS:", arch_side)
+			for ic in (temp_ic, reference, probe):
+				ic.add_noise(noise_ic, arch_side, noise_hd if noise_hd > 0 else None)
+				ic.remove_top_and_bottom_rows(median_filter_rows)
+				ic.expand(2)
+
+			shift = int(np.rint(2 * np.random.randn() + 2))
+			print ("S:", shift)
+			probe.shift(shift)
+
+			print (reference.hamming_distance(probe, rotations=8))
+			print (reference.hamming_distance(probe, rotations=8, mask=True))
+			print (probe._template.shape)
+			self._produced += 1
+			return reference, probe
+		else:
+			raise StopIteration
 
 def weibull(shape, m=0.0001, t_min=0.015, t_max=0.25):
 	normalise = lambda o_min, o_max, t_min, t_max, value: ((t_max - t_min) / (o_max - o_min)) * (value - o_max) + t_max
@@ -80,6 +122,7 @@ def flip_templates(temp_ic, template_hd):
 	gspace = np.geomspace(0.15, 0.05, num=temp_ic._template.shape[0])
 	hd = 0.0
 	reference, probe = copy.deepcopy(temp_ic), copy.deepcopy(temp_ic)
+	z = 0
 	while not (math.isclose(hd, template_hd, abs_tol=0.005) or template_hd < hd):
 		reference.majority_vote()
 		probe.majority_vote()
@@ -97,51 +140,34 @@ def flip_templates(temp_ic, template_hd):
 		test_probe = copy.deepcopy(probe)
 		test_reference.medfilt()
 		test_probe.medfilt()
+		z+=1
 		hd, _, _ = test_reference.hamming_distance(test_probe, 0, cut_rows=median_filter_rows)
 	return test_reference, test_probe, hd
+	
+def produce(subdirectories: List[int]):
+	'''Produce iris-codes segragated into subdirectories.'''
+	for subject_num, (reference, probe) in enumerate(IrisCodeGenerator(len(subdirectories), 0)):
+		save_dir = generated_directory / Path(str(subdirectories[subject_num]))
+		reference.to_image(save_dir / Path("1.bmp"))
+		probe.to_image(save_dir / Path("2.bmp"))
 
 if __name__ == '__main__':
 	set_start_method("spawn")
-	reference_generation_hd = 0.4625
+	start = timer()
+	try:
+		num_pools = cpus if cpus <= cpu_count() else cpu_count
+	except NotImplementedError:
+		num_pools = 1
 
-	temp_ic = Template.create(32, 512, 6.5, 0.25)
-	temp_ic.to_image(Path("test.bmp"))
-	temp_ic.shift(np.random.randint(10, 512 // 2))
-	temp_ic.initial_zigzag()
+	subdirectories = np.array_split(range(1, subjects+1), num_pools)
+	if num_pools > 1:
+		with Pool(num_pools) as p:
+			p.map(produce, subdirectories)
+	else:
+		produce(subdirectories[0])
 
-	seqs = temp_ic.find_sequences_of_all(1)
-	target_hd = float(weibull(6))
-	exp_overlap = expected_overlap(target_hd)
-	i_hd = (target_hd + exp_overlap) / 2
-	b_hd = reference_generation_hd - i_hd
-
-	print ("T-HD:", target_hd)
-	print ("I-HD:", i_hd)
-	print ("B-HD:", b_hd)
-	temp_ic = flip_barcode(temp_ic, b_hd)
-
-	reference, probe, a_hd = flip_templates(temp_ic, target_hd)
-
-	noise_hd = target_hd - a_hd
-	print ("A-HD:", a_hd)
-	print ("BR-HD:", temp_ic.hamming_distance(reference)[0])
-	print ("BP-HD:", temp_ic.hamming_distance(probe)[0])
-	print ("N-HD:", noise_hd)
-	arch_side = np.random.choice(("l", "r", None), p=(0.5, 0.5, 0.0))
-	print ("AS:", arch_side)
-	for ic in (temp_ic, reference, probe):
-		ic.add_noise(noise_ic, arch_side, noise_hd if noise_hd > 0 else None)
-		ic.remove_top_and_bottom_rows(median_filter_rows)
-		ic.expand(2)
-
-	shift = int(np.rint(2 * np.random.randn() + 2))
-	print ("S:", shift)
-	probe.shift(shift)
-
-	print (reference.hamming_distance(probe, rotations=8))
-	print (reference.hamming_distance(probe, rotations=8, mask=True))
-	print (probe._template.shape)
-	temp_ic.to_image(Path("b.bmp"))
-	reference.to_image(Path("r.bmp"))
-	probe.to_image(Path("p.bmp"))
-	quit()
+	stop = timer()
+	elapsed = stop - start
+	m, s = divmod(elapsed, 60)
+	h, m = divmod(m, 60)
+	print ("Time elapsed: %d:%02d:%02d" % (h, m, s))
