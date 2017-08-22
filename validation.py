3ae6e1e356687eb80ca0d5bdc68f7a692a2512a5
Pawel Drozdowski <pawel.drozdowski@h-da.de>
2017-08-22 12:41:58 +0200

    fix for impostor pair generation

--- validation.py
+++ validation.py
@@ -43,7 +43,13 @@ def hamming_distance_validation(processes: int, ic_sample: List[np.ndarray], rot
 	# Prepare the pairs of templates to compare
 	pairs_genuine = [pair for pair in itertools.combinations(ic_sample, 2) if pair[0][0] == pair[1][0] and pair[0][1] != pair[1][1]]
 	logging.debug("Created genuine template pairs")
-	pairs_impostor = [pair for i, pair in enumerate(itertools.combinations(ic_sample, 2)) if i < max_impostor_comparisons and pair[0][0] != pair[1][0] and pair[0][1] != "2" and pair[1][1] != "2"]
+	pairs_impostor = []
+	for pair in itertools.combinations(ic_sample, 2):
+		if pair[0][0] != pair[1][0] and pair[0][1] != "2" and pair[1][1] != "2":
+			pairs_impostor.append(pair)
+			i +=1
+		if i >= max_impostor_comparisons:
+			break
 	logging.debug("Created impostor template pairs")
 
 	# Compute the HDs
